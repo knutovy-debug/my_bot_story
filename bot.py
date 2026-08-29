d4e9bc8dbbf89d30652d0542ea3eeadc05215dfc
knutovy-debug <knutovy@gmail.com>
2026-08-29 12:24:00 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ CARD_NUMBER = "2202208186522703"
 DONATE_LINK = "2202208186522703"
 MONTHLY_PRICE = "299 рублей"
 YEARLY_PRICE = "1990 рублей"
-ADMIN_ID = "ТВОЙ_ID"  # <--- ВСТАВЬ СВОЙ ID СЮДА (узнаёшь у @userinfobot)
+ADMIN_ID = "1177629279"  # <--- ВСТАВЬ СВОЙ ID СЮДА (узнаёшь у @userinfobot)
 
 # ======== ИНИЦИАЛИЗАЦИЯ DEEPSEEK ========
 client = OpenAI(
