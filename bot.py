de1e2fc01fa51bb76d7cd7bf1f8ca0363877e533
knutovy-debug <knutovy@gmail.com>
2026-08-30 15:31:17 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -505,7 +505,7 @@ conv = ConversationHandler(
     fallbacks=[CommandHandler("cancel", cancel)]
 )
 app.add_handler(conv)
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_buttons))
+app.add_handler(MessageHandler(filters.Regex("оплатил") & ~filters.COMMAND, payment_message_handler))
 app.add_handler(CallbackQueryHandler(payment_handler))
 app.add_handler(CallbackQueryHandler(confirm_handler))
 
