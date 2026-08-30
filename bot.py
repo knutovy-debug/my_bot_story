6da3bba245a2c3629606f75f82932c73b50b5e72
knutovy-debug <knutovy@gmail.com>
2026-08-30 11:06:44 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -499,7 +499,7 @@ conv = ConversationHandler(
 )
 app.add_handler(conv)
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_buttons))
-app.add_handler(MessageHandler(filters.Regex("оплатил") & ~filters.COMMAND, payment_message_handler))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, payment_message_handler))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_buttons))
 app.add_handler(CallbackQueryHandler(payment_handler))
 app.add_handler(CallbackQueryHandler(confirm_handler))
